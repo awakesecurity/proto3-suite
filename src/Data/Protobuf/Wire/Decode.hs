9821abc233ae6c4c35824e77c81c2ad453d87250
Connor Clark <connor@awakenetworks.com>
2016-04-12 14:32:36 -0700

    re-export Parser from Decode

--- src/Data/Protobuf/Wire/Decode.hs
+++ src/Data/Protobuf/Wire/Decode.hs
@@ -2,6 +2,9 @@ module Data.Protobuf.Wire.Decode
 (
 -- * Untyped Representation
 getTuples
+-- * Parser interface
+, module Data.Protobuf.Wire.Decode.Parser
 ) where
 
 import Data.Protobuf.Wire.Decode.Internal
+import Data.Protobuf.Wire.Decode.Parser
