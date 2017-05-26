334f3d6d3c9477ad80a753d7e8716edac65e1e07
Joel Stanley <intractable@gmail.com>
2017-05-26 11:38:37 -0500

    Add pretty-show dep so it can be used in the repl

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, bytestring, cereal, containers, deepseq
-, filepath, haskell-src, mtl, parsec, parsers, pretty, proto3-wire
-, QuickCheck, safe, semigroups, stdenv, tasty, tasty-hunit
-, tasty-quickcheck, text, transformers, turtle, vector
+, filepath, haskell-src, mtl, parsec, parsers, pretty, pretty-show
+, proto3-wire, QuickCheck, safe, semigroups, stdenv, tasty
+, tasty-hunit, tasty-quickcheck, text, transformers, turtle, vector
 }:
 mkDerivation {
   pname = "proto3-suite";
@@ -9,12 +9,13 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [
     base bytestring cereal containers deepseq filepath haskell-src mtl
-    parsec parsers pretty proto3-wire QuickCheck safe semigroups text
-    transformers vector
+    parsec parsers pretty pretty-show proto3-wire QuickCheck safe
+    semigroups text transformers vector
   ];
   testHaskellDepends = [
-    base bytestring cereal proto3-wire QuickCheck semigroups tasty
-    tasty-hunit tasty-quickcheck text transformers turtle vector
+    base bytestring cereal pretty-show proto3-wire QuickCheck
+    semigroups tasty tasty-hunit tasty-quickcheck text transformers
+    turtle vector
   ];
   description = "A low level library for writing out data in the Protocol Buffers wire format";
   license = stdenv.lib.licenses.asl20;
