da8baae30b101a79ccc5844ece5920d801eced91
Joel Stanley <intractable@gmail.com>
2017-08-17 16:52:35 -0500

    Update `generate-test-types.sh` and re-run CG

--- tests/generate-test-types.sh
+++ tests/generate-test-types.sh
@@ -1,3 +1,3 @@
 PROTO3_SUITE_NO_TESTS=$(nix-build --no-out-link ../release.nix -A proto3-suite-no-tests)
-"${PROTO3_SUITE_NO_TESTS}"/bin/compile-proto-file --includeDir ../test-files --proto test_proto.proto        > TestProto.hs
-"${PROTO3_SUITE_NO_TESTS}"/bin/compile-proto-file --includeDir ../test-files --proto test_proto_import.proto > TestProtoImport.hs
+"${PROTO3_SUITE_NO_TESTS}"/bin/compile-proto-file --out . --includeDir ../test-files --proto test_proto.proto
+"${PROTO3_SUITE_NO_TESTS}"/bin/compile-proto-file --out . --includeDir ../test-files --proto test_proto_import.proto
