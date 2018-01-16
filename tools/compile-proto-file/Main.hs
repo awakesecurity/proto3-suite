bb66853dcaa7372ef061cf83f39e4ba5193898de
gbaz <gershomb@gmail.com>
2018-01-16 13:41:30 -0500

    Make instance generation extensible (#42)
    * allow file to override instance creation
    
    * Update Main.hs
    
    update docstring
    
    * handle namespacing

--- tools/compile-proto-file/Main.hs
+++ tools/compile-proto-file/Main.hs
@@ -17,6 +17,7 @@ data Args w = Args
   { out        :: w ::: FilePath   <?> "Output directory path where generated Haskell modules will be written (directory is created if it does not exist; note that files in the output directory may be overwritten!)"
   , includeDir :: w ::: [FilePath] <?> "Path to search for included .proto files (can be repeated, and paths will be searched in order; the current directory is used if this option is not provided)"
   , proto      :: w ::: FilePath   <?> "Path to input .proto file"
+  , extraInstanceFile :: w ::: [FilePath] <?> "Additional file to provide instances that would otherwise be generated. Can be used multiple times. Types for which instance overrides are given must be fully qualified."
   } deriving Generic
 instance ParseRecord (Args Wrapped)
 deriving instance Show (Args Unwrapped)
@@ -24,4 +25,4 @@ deriving instance Show (Args Unwrapped)
 main :: IO ()
 main = do
   Args{..} :: Args Unwrapped <- unwrapRecord "Compiles a .proto file to a Haskell module"
-  compileDotProtoFileOrDie out includeDir proto
+  compileDotProtoFileOrDie extraInstanceFile out includeDir proto
