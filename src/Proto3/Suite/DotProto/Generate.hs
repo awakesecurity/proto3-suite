9be20e0134a4325c38a456b9a18f25f67d2ae80a
joshvera <josh@joshvera.com>
2018-07-02 14:12:17 -0400

    Convert namespaced term names to single names in the environment

--- src/Proto3/Suite/DotProto/Generate.hs
+++ src/Proto3/Suite/DotProto/Generate.hs
@@ -60,6 +60,7 @@ import           Turtle                         (FilePath)
 import qualified Turtle
 import           Turtle.Format                  ((%))
 import qualified Turtle.Format                  as F
+import qualified Data.Char as Char
 
 -- * Public interface
 
@@ -378,11 +379,20 @@ hsTypeFromDotProtoPrim _    Bool            = pure $ primType_ "Bool"
 hsTypeFromDotProtoPrim _    Float           = pure $ primType_ "Float"
 hsTypeFromDotProtoPrim _    Double          = pure $ primType_ "Double"
 hsTypeFromDotProtoPrim ctxt (Named msgName) =
-    case M.lookup msgName ctxt of
+    let
+      msgName' = case msgName of
+        (Dots (Path [_, name])) -> Single name
+        name -> name
+    in
+    case M.lookup msgName' ctxt of
       Just ty@(DotProtoTypeInfo { dotProtoTypeInfoKind = DotProtoKindEnum }) ->
-          HsTyApp (protobufType_ "Enumerated") <$> msgTypeFromDpTypeInfo ty msgName
-      Just ty -> msgTypeFromDpTypeInfo ty msgName
+          HsTyApp (protobufType_ "Enumerated") <$> msgTypeFromDpTypeInfo ty msgName'
+      Just ty -> msgTypeFromDpTypeInfo ty msgName'
       Nothing -> noSuchTypeError msgName
+
+capitalized :: String -> String
+capitalized (head:tail) = Char.toUpper head : map Char.toLower tail
+capitalized [] = []
 
 -- | Generate the Haskell type name for a 'DotProtoTypeInfo' for a message /
 --   enumeration being compiled. NB: We ignore the 'dotProtoTypeInfoPackage'
