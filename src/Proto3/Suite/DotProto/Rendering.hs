2d2d29c3bdd79c58a3d8351ba70e17d32f14b376
Timothy Clem <timothy.clem@gmail.com>
2018-07-03 14:18:31 -0700

    Switch over to wl-pprint for easier formatting, fix formatting

--- src/Proto3/Suite/DotProto/Rendering.hs
+++ src/Proto3/Suite/DotProto/Rendering.hs
@@ -1,6 +1,5 @@
 -- | This module provides types and functions to generate .proto files.
 
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedLists            #-}
 {-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
@@ -21,13 +20,12 @@ module Proto3.Suite.DotProto.Rendering
   ) where
 
 import           Data.Char
-import qualified Data.Text                       as T
-import           Filesystem.Path.CurrentOS       (toText)
+import qualified Data.Text                 as T
+import           Filesystem.Path.CurrentOS (toText)
 import           Proto3.Suite.DotProto.AST
-import           Proto3.Wire.Types               (FieldNumber (..))
-import           Text.PrettyPrint                (($$), (<+>), (<>))
-import qualified Text.PrettyPrint                as PP
-import           Text.PrettyPrint.HughesPJClass  (Pretty(..))
+import           Proto3.Wire.Types         (FieldNumber (..))
+import           Text.PrettyPrint.Leijen   ((<$$>), (<+>), (<>), Pretty(..))
+import qualified Text.PrettyPrint.Leijen   as PP
 
 -- | Options for rendering a @.proto@ file.
 data RenderingOptions = RenderingOptions
@@ -50,82 +48,91 @@ defRenderingOptions =
 
 -- | The default choice of field name for a selector.
 defSelectorName :: DotProtoIdentifier -> DotProtoIdentifier -> FieldNumber -> PP.Doc
-defSelectorName _ fieldName _ = pPrint fieldName
+defSelectorName _ fieldName _ = pretty fieldName
 
 -- | The default choice of enum member name for an enum
 defEnumMemberName :: DotProtoIdentifier -> DotProtoIdentifier -> PP.Doc
-defEnumMemberName = const pPrint
+defEnumMemberName = const pretty
 
 -- | Traverses a DotProto AST and generates a .proto file from it
 renderDotProto :: RenderingOptions -> DotProto -> PP.Doc
 renderDotProto opts DotProto{..}
-  = PP.text "syntax = \"proto3\";"
- $$ pPrint protoPackage
- $$ (PP.vcat $ pPrint    <$> protoImports)
- $$ (PP.vcat $ topOption <$> protoOptions)
- $$ (PP.vcat $ prettyPrintProtoDefinition opts <$> protoDefinitions)
+  = PP.text "syntax = \"proto3\";" <> PP.linebreak
+ <$$> pretty protoPackage <> PP.linebreak
+ <>   pList pretty protoImports
+ <>   pList topOption protoOptions
+ <$$> (PP.vcat . PP.punctuate PP.linebreak $ (prettyPrintProtoDefinition opts) <$> protoDefinitions)
+ <$$> PP.linebreak
+ where pList f [] = PP.empty
+       pList f xs =  PP.linebreak
+                  <> PP.vcat (f <$> xs)
+                  <> PP.linebreak
 
 instance Pretty DotProtoPackageSpec where
-  pPrint (DotProtoPackageSpec p) = PP.text "package" <+> pPrint p <> PP.text ";"
-  pPrint (DotProtoNoPackage)     = PP.empty
+  pretty (DotProtoPackageSpec p) = PP.text "package" <+> pretty p <> PP.semi
+  pretty (DotProtoNoPackage)     = PP.empty
 
 instance Pretty DotProtoImport where
-  pPrint (DotProtoImport q i) =
-    PP.text "import" <+> pPrint q <+> PP.doubleQuotes (PP.text fp) <> PP.text ";"
+  pretty (DotProtoImport q i) = pretty q <+> PP.dquotes (PP.text fp) <> PP.semi
     where
       fp = case T.unpack . either id id . toText $ i of
              [] -> show ("" :: String)
              x  -> x
 
 instance Pretty DotProtoImportQualifier where
-  pPrint DotProtoImportDefault = PP.empty
-  pPrint DotProtoImportPublic  = PP.text "public"
-  pPrint DotProtoImportWeak    = PP.text "weak"
+  pretty DotProtoImportDefault = PP.text "import"
+  pretty DotProtoImportPublic  = PP.text "import public"
+  pretty DotProtoImportWeak    = PP.text "import weak"
 
 optionAnnotation :: [DotProtoOption] -> PP.Doc
 optionAnnotation [] = PP.empty
-optionAnnotation os = PP.brackets
+optionAnnotation os = (<> PP.space)
+                    . PP.brackets
                     . PP.hcat
                     . PP.punctuate (PP.text ", ")
-                    $ pPrint <$> os
+                    $ pretty <$> os
 
 topOption :: DotProtoOption -> PP.Doc
-topOption o = PP.text "option" <+> pPrint o <> PP.text ";"
+topOption o = PP.text "option" <+> pretty o <> PP.semi
 
 instance Pretty DotProtoOption where
-  pPrint (DotProtoOption key value) = pPrint key <+> PP.text "=" <+> pPrint value
+  pretty (DotProtoOption key value) = pretty key <+> PP.text "=" <+> pretty value
 
 prettyPrintProtoDefinition :: RenderingOptions -> DotProtoDefinition -> PP.Doc
 prettyPrintProtoDefinition opts = defn where
   defn :: DotProtoDefinition -> PP.Doc
-  defn (DotProtoMessage name parts) = PP.text "message" <+> pPrint name <+> (braces $ PP.vcat $ msgPart name <$> parts)
-  defn (DotProtoEnum    name parts) = PP.text "enum"    <+> pPrint name <+> (PP.braces $ PP.vcat $ enumPart name <$> parts)
-  defn (DotProtoService name parts) = PP.text "service" <+> pPrint name <+> (PP.braces $ PP.vcat $ pPrint <$> parts)
+  defn (DotProtoMessage name parts) = PP.text "message" <+> pretty name <+> vcat (msgPart name <$> parts)
+  defn (DotProtoEnum    name parts) = PP.text "enum"    <+> pretty name <+> vcat (enumPart name <$> parts)
+  defn (DotProtoService name parts) = PP.text "service" <+> pretty name <+> vcat (pretty <$> parts)
 
-  -- Put the final closing brace on the next line.
-  -- This is important, since the final field might have a comment, and
-  -- the brace cannot be part of the comment.
-  -- We could use block comments instead, once the parser/lexer supports them.
-  braces :: PP.Doc -> PP.Doc
-  braces = ($$ PP.text "}") . (PP.text "{" <+>)
+  -- Vertical concat with braces that appropriately handles empty list (prints
+  -- `{ }` on one line) and handles nesting and trailing newlines. Also puts the
+  -- final closing brace on the next line since the final field might have a
+  -- comment, and the brace cannot be part of the comment. We could use block
+  -- comments instead, once the parser/lexer supports them.
+  vcat :: [PP.Doc] -> PP.Doc
+  vcat [] = PP.braces (PP.char ' ')
+  vcat xs =    PP.char '{'
+          <$$> PP.indent 2 (PP.vcat xs)
+          <$$> PP.char '}'
 
   msgPart :: DotProtoIdentifier -> DotProtoMessagePart -> PP.Doc
-  msgPart msgName (DotProtoMessageField f)           = field msgName f
+  msgPart msgName (DotProtoMessageField f)               = field msgName f
   msgPart _       (DotProtoMessageDefinition definition) = defn definition
   msgPart _       (DotProtoMessageReserved reservations)
     =   PP.text "reserved"
-    <+> (PP.hcat . PP.punctuate (PP.text ", ") $ pPrint <$> reservations)
-    <>  PP.text ";"
-  msgPart msgName (DotProtoMessageOneOf name fields)     = PP.text "oneof" <+> pPrint name <+> (PP.braces $ PP.vcat $ field msgName <$> fields)
+    <+> (PP.hcat . PP.punctuate (PP.text ", ") $ pretty <$> reservations)
+    <>  PP.semi
+  msgPart msgName (DotProtoMessageOneOf name fields)     = PP.text "oneof" <+> pretty name <+> vcat (field msgName <$> fields)
 
   field :: DotProtoIdentifier -> DotProtoField -> PP.Doc
   field msgName (DotProtoField number mtype name options comments)
-    =   pPrint mtype
+    =   pretty mtype
     <+> roSelectorName opts msgName name number
     <+> PP.text "="
-    <+> pPrint number
-    <+> optionAnnotation options
-    <>  PP.text ";"
+    <+> pretty number
+    <>  optionAnnotation options
+    <>  PP.semi
     <>  maybe PP.empty (PP.text . (" // " ++)) comments
   field _ DotProtoEmptyField = PP.empty
 
@@ -133,80 +140,80 @@ prettyPrintProtoDefinition opts = defn where
   enumPart msgName (DotProtoEnumField name value options)
     = roEnumMemberName opts msgName name
     <+> PP.text "="
-    <+> pPrint value
-    <+> optionAnnotation options
-    <> PP.text ";"
+    <+> pretty value
+    <> optionAnnotation options
+    <> PP.semi
   enumPart _       (DotProtoEnumOption opt)
-    = PP.text "option" <+> pPrint opt <> PP.text ";"
+    = PP.text "option" <+> pretty opt <> PP.semi
   enumPart _       DotProtoEnumEmpty
     = PP.empty
 
 instance Pretty DotProtoServicePart where
-  pPrint (DotProtoServiceRPC name (callname, callstrm) (retname, retstrm) options)
+  pretty (DotProtoServiceRPC name (callname, callstrm) (retname, retstrm) options)
     =   PP.text "rpc"
-    <+> pPrint name
-    <+> PP.parens (pPrint callstrm <+> pPrint callname)
+    <+> pretty name
+    <+> PP.parens (pretty callstrm <+> pretty callname)
     <+> PP.text "returns"
-    <+> PP.parens (pPrint retstrm <+> pPrint retname)
-    <+> case options of
-          [] -> PP.text ";"
-          _  -> PP.braces . PP.vcat $ topOption <$> options
-  pPrint (DotProtoServiceOption option) = topOption option
-  pPrint DotProtoServiceEmpty           = PP.empty
+    <+> PP.parens (pretty retstrm <+> pretty retname)
+    <>  case options of
+          [] -> PP.semi
+          _  -> PP.space <> (PP.braces . PP.vcat $ topOption <$> options)
+  pretty (DotProtoServiceOption option) = topOption option
+  pretty DotProtoServiceEmpty           = PP.empty
 
 instance Pretty Streaming where
-  pPrint Streaming    = PP.text "stream"
-  pPrint NonStreaming = PP.empty
+  pretty Streaming    = PP.text "stream"
+  pretty NonStreaming = PP.empty
 
 instance Pretty DotProtoIdentifier where
-  pPrint (Single name)                    = PP.text name
-  pPrint (Dots (Path names))              = PP.hcat . PP.punctuate (PP.text ".") $ PP.text <$> names
-  pPrint (Qualified qualifier identifier) = PP.parens (pPrint qualifier) <> PP.text "." <> pPrint identifier
-  pPrint Anonymous                        = PP.empty
+  pretty (Single name)                    = PP.text name
+  pretty (Dots (Path names))              = PP.hcat . PP.punctuate PP.dot $ PP.text <$> names
+  pretty (Qualified qualifier identifier) = PP.parens (pretty qualifier) <> PP.dot <> pretty identifier
+  pretty Anonymous                        = PP.empty
 
 instance Pretty DotProtoValue where
-  pPrint (Identifier value) = pPrint value
-  pPrint (StringLit  value) = PP.text $ show value
-  pPrint (IntLit     value) = PP.text $ show value
-  pPrint (FloatLit   value) = PP.text $ show value
-  pPrint (BoolLit    value) = PP.text $ toLower <$> show value
+  pretty (Identifier value) = pretty value
+  pretty (StringLit  value) = PP.text $ show value
+  pretty (IntLit     value) = PP.text $ show value
+  pretty (FloatLit   value) = PP.text $ show value
+  pretty (BoolLit    value) = PP.text $ toLower <$> show value
 
 instance Pretty DotProtoType where
-  pPrint (Prim           ty) = pPrint ty
-  pPrint (Optional       ty) = pPrint ty
-  pPrint (Repeated       ty) = PP.text "repeated" <+> pPrint ty
-  pPrint (NestedRepeated ty) = PP.text "repeated" <+> pPrint ty
-  pPrint (Map keyty valuety) = PP.text "map<" <> pPrint keyty <> PP.text ", " <> pPrint valuety <> PP.text ">"
+  pretty (Prim           ty) = pretty ty
+  pretty (Optional       ty) = pretty ty
+  pretty (Repeated       ty) = PP.text "repeated" <+> pretty ty
+  pretty (NestedRepeated ty) = PP.text "repeated" <+> pretty ty
+  pretty (Map keyty valuety) = PP.text "map<" <> pretty keyty <> PP.text ", " <> pretty valuety <> PP.text ">"
 
 instance Pretty DotProtoPrimType where
-  pPrint (Named i)  = pPrint i
-  pPrint Int32      = PP.text "int32"
-  pPrint Int64      = PP.text "int64"
-  pPrint SInt32     = PP.text "sint32"
-  pPrint SInt64     = PP.text "sint64"
-  pPrint UInt32     = PP.text "uint32"
-  pPrint UInt64     = PP.text "uint64"
-  pPrint Fixed32    = PP.text "fixed32"
-  pPrint Fixed64    = PP.text "fixed64"
-  pPrint SFixed32   = PP.text "sfixed32"
-  pPrint SFixed64   = PP.text "sfixed64"
-  pPrint String     = PP.text "string"
-  pPrint Bytes      = PP.text "bytes"
-  pPrint Bool       = PP.text "bool"
-  pPrint Float      = PP.text "float"
-  pPrint Double     = PP.text "double"
+  pretty (Named i)  = pretty i
+  pretty Int32      = PP.text "int32"
+  pretty Int64      = PP.text "int64"
+  pretty SInt32     = PP.text "sint32"
+  pretty SInt64     = PP.text "sint64"
+  pretty UInt32     = PP.text "uint32"
+  pretty UInt64     = PP.text "uint64"
+  pretty Fixed32    = PP.text "fixed32"
+  pretty Fixed64    = PP.text "fixed64"
+  pretty SFixed32   = PP.text "sfixed32"
+  pretty SFixed64   = PP.text "sfixed64"
+  pretty String     = PP.text "string"
+  pretty Bytes      = PP.text "bytes"
+  pretty Bool       = PP.text "bool"
+  pretty Float      = PP.text "float"
+  pretty Double     = PP.text "double"
 
 instance Pretty FieldNumber where
-  pPrint = PP.text . show . getFieldNumber
+  pretty = PP.text . show . getFieldNumber
 
 instance Pretty DotProtoReservedField where
-  pPrint (SingleField num)      = PP.text $ show num
-  pPrint (FieldRange start end) = (PP.text $ show start) <+> PP.text "to" <+> (PP.text $ show end)
-  pPrint (ReservedIdentifier i) = PP.text $ show i
+  pretty (SingleField num)      = PP.text $ show num
+  pretty (FieldRange start end) = (PP.text $ show start) <+> PP.text "to" <+> (PP.text $ show end)
+  pretty (ReservedIdentifier i) = PP.text $ show i
 
 -- | Render protobufs metadata as a .proto file stringy
 toProtoFile :: RenderingOptions -> DotProto -> String
-toProtoFile opts = PP.render . renderDotProto opts
+toProtoFile opts d = PP.displayS ((PP.renderPretty 0.4 100) (renderDotProto opts d)) ""
 
 -- | Render protobufs metadata as a .proto file string,
 -- using the default rendering options.
