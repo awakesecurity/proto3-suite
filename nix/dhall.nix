bc1493e63fa40d4c1154bd4e1b6913f596c2ae12
Gershom <gershom@awakesecurity.com>
2019-02-28 15:52:36 -0500

    bump dhall dep

--- nix/dhall.nix
+++ nix/dhall.nix
@@ -1,33 +1,40 @@
-{ mkDerivation, ansi-terminal, base, bytestring, case-insensitive
-, containers, contravariant, criterion, cryptonite, deepseq, Diff
-, directory, doctest, exceptions, filepath, haskeline, http-client
-, http-client-tls, insert-ordered-containers, lens-family-core
-, megaparsec, memory, mockery, mtl, optparse-applicative, parsers
-, prettyprinter, prettyprinter-ansi-terminal, repline, scientific
-, stdenv, tasty, tasty-hunit, template-haskell, text, transformers
-, unordered-containers, vector
+{ mkDerivation, aeson, aeson-pretty, ansi-terminal, base
+, bytestring, case-insensitive, cborg, cborg-json, containers
+, contravariant, criterion, cryptonite, deepseq, Diff, directory
+, doctest, dotgen, exceptions, filepath, haskeline, http-client
+, http-client-tls, http-types, lens-family-core, megaparsec, memory
+, mockery, mtl, optparse-applicative, parsers, prettyprinter
+, prettyprinter-ansi-terminal, QuickCheck, quickcheck-instances
+, repline, scientific, serialise, stdenv, tasty, tasty-hunit
+, tasty-quickcheck, template-haskell, text, transformers
+, unordered-containers, uri-encode, vector
 }:
 mkDerivation {
   pname = "dhall";
-  version = "1.15.1";
-  sha256 = "ea3bb3b6a006b82e0286d72757ba9969f6ee292ef12eea2376939ba219c88e30";
+  version = "1.20.1";
+  sha256 = "e077e8f4945484db4e35e8ae422e9eabac1b155972602f0404d818e3e185bcdc";
+  revision = "2";
+  editedCabalFile = "0629z8lc97rapfcqcgvxwp9x4x3xqpzrly8m0nsn0dds7400jxrk";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    ansi-terminal base bytestring case-insensitive containers
-    contravariant cryptonite Diff directory exceptions filepath
-    haskeline http-client http-client-tls insert-ordered-containers
-    lens-family-core megaparsec memory mtl optparse-applicative parsers
-    prettyprinter prettyprinter-ansi-terminal repline scientific
-    template-haskell text transformers unordered-containers vector
+    aeson aeson-pretty ansi-terminal base bytestring case-insensitive
+    cborg cborg-json containers contravariant cryptonite Diff directory
+    dotgen exceptions filepath haskeline http-client http-client-tls
+    http-types lens-family-core megaparsec memory mtl
+    optparse-applicative parsers prettyprinter
+    prettyprinter-ansi-terminal repline scientific serialise
+    template-haskell text transformers unordered-containers uri-encode
+    vector
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    base deepseq directory doctest filepath insert-ordered-containers
-    mockery prettyprinter tasty tasty-hunit text vector
+    base containers deepseq directory doctest filepath mockery
+    prettyprinter QuickCheck quickcheck-instances serialise tasty
+    tasty-hunit tasty-quickcheck text transformers vector
   ];
   benchmarkHaskellDepends = [
-    base containers criterion directory text
+    base bytestring containers criterion directory serialise text
   ];
   description = "A configuration language guaranteed to terminate";
   license = stdenv.lib.licenses.bsd3;
