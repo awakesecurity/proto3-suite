awakesecurity/proto3-suite

commit 4007fd0351a0629174da656fa02ecd76fac965eb

type constructor param is f by convention

src/Proto3/Suite/Class.hs
@@ -224,10 +224,10 @@ instance HasDefault (M.Map k v) where
   def = M.empty
   isDefault = M.null
 
-class GenericHasDefault (a :: * -> *) where
-  genericDef :: a x
-instance HasDefault a => GenericHasDefault (K1 i a) where
-  genericDef = K1 (def @a)
+class GenericHasDefault (f :: * -> *) where
+  genericDef :: f x
+instance HasDefault a => GenericHasDefault (K1 i f) where
+  genericDef = K1 (def @f)
 instance (GenericHasDefault f, GenericHasDefault g) => GenericHasDefault (f :*: g) where
   genericDef = genericDef @f :*: genericDef @g
 instance (GenericHasDefault f, GenericHasDefault g) => GenericHasDefault (f :+: g) where

commit 88c19d5da7ee7a04e445e59b04f2ddeea9b41e95

fix it (#108)
fix error introduced in 4007fd0351a0629174da656fa02ecd76fac965eb

src/Proto3/Suite/Class.hs
@@ -226,7 +226,7 @@ instance HasDefault (M.Map k v) where
 
 class GenericHasDefault (f :: * -> *) where
   genericDef :: f x
-instance HasDefault a => GenericHasDefault (K1 i f) where
+instance HasDefault f => GenericHasDefault (K1 i f) where
   genericDef = K1 (def @f)
 instance (GenericHasDefault f, GenericHasDefault g) => GenericHasDefault (f :*: g) where
   genericDef = genericDef @f :*: genericDef @g