8448a649fa38e545f415e1c5d5cd5a119163a0e6
John C. Carey <jcarey@awakesecurity.com>
2019-06-17 22:56:20 -0700

    Improve compatibility across GHC versions.

--- src/Proto3/Suite/DotProto/Internal.hs
+++ src/Proto3/Suite/DotProto/Internal.hs
@@ -30,7 +30,7 @@ import           Data.List                 (find, intercalate)
 import qualified Data.List.NonEmpty        as NE
 import qualified Data.Map                  as M
 import           Data.Maybe                (fromMaybe)
-import           Data.Monoid
+import           Data.Semigroup            (Semigroup(..))
 import qualified Data.Text                 as T
 import           Data.Tuple                (swap)
 import           Filesystem.Path.CurrentOS ((</>))
@@ -60,7 +60,8 @@ liftEither = either throwError pure
 #endif
 
 -- | Like 'foldMap', but with an effectful projection.
-foldMapM :: (Foldable t, Monad m, Monoid b) => (a -> m b) -> t a -> m b
+foldMapM ::
+  (Foldable t, Monad m, Monoid b, Semigroup b) => (a -> m b) -> t a -> m b
 foldMapM f = foldM (\b a -> (b <>) <$> f a) mempty
 
 -- | Like 'Control.Lens.Getter.Getting', but allows for retrieving the 'r'
