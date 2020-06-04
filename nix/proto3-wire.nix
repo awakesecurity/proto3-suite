1d7fdce5418821130c8674f73b140b6fd8f3790d
j6carey <jcarey@awakesecurity.com>
2020-06-03 19:03:50 -0700

    Upgrade to proto3-wire 1.2.0. (#127)
    Also raise the minimum GHC version to 8.6 and
    drop support for Nix versions prior to 1.12.

--- nix/proto3-wire.nix
+++ nix/proto3-wire.nix
@@ -1,22 +1,25 @@
 { mkDerivation, base, bytestring, cereal, containers, deepseq
-, doctest, fetchgit, hashable, QuickCheck, safe, stdenv, tasty
-, tasty-hunit, tasty-quickcheck, text, unordered-containers
+, doctest, fetchgit, ghc-prim, hashable, primitive, QuickCheck
+, safe, stdenv, tasty, tasty-hunit, tasty-quickcheck, text
+, transformers, unordered-containers, vector
 }:
 mkDerivation {
   pname = "proto3-wire";
-  version = "1.1.0";
+  version = "1.2.0";
   src = fetchgit {
     url = "https://github.com/awakesecurity/proto3-wire";
-    sha256 = "16l1rnnygwk1b2sb3l6klhr6ad0wvry204icxnc81c6rbzbk6rqc";
-    rev = "4f355bbac895d577d8a28f567ab4380f042ccc24";
+    sha256 = "0rhf20yhqg141riyix5s3vpypfn9xz010snhn62lhswyhpb6g5zp";
+    rev = "cdf19f50929657c37e866f6937e7cc8fe8624983";
+    fetchSubmodules = true;
   };
   libraryHaskellDepends = [
-    base bytestring cereal containers deepseq hashable QuickCheck safe
-    text unordered-containers
+    base bytestring cereal containers deepseq ghc-prim hashable
+    primitive QuickCheck safe text transformers unordered-containers
+    vector
   ];
   testHaskellDepends = [
     base bytestring cereal doctest QuickCheck tasty tasty-hunit
-    tasty-quickcheck text
+    tasty-quickcheck text transformers vector
   ];
   description = "A low-level implementation of the Protocol Buffers (version 3) wire format";
   license = stdenv.lib.licenses.asl20;
