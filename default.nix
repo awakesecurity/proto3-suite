35806545ad712dd8aae7de9d9845330a9fd14372
Evan Relf <evan@awakesecurity.com>
2021-06-22 10:40:37 -0700

    Remove unused `{linux,darwin}Pkgs` attributes

--- default.nix
+++ default.nix
@@ -1,19 +1,13 @@
 { compiler ? "ghc884", enableDhall ? false }:
 
 let
-  config = { allowBroken = true; };
+  pkgs = import ./nix/nixpkgs.nix {
+    config = { allowBroken = true; };
 
-  overlays = [
-    (import ./nix/overlays/haskell.nix { inherit compiler enableDhall; })
-  ];
-
-  pkgs = import ./nix/nixpkgs.nix { inherit config overlays; };
-
-  linuxPkgs =
-    import ./nix/nixpkgs.nix { inherit config overlays; system = "x86_64-linux" ; };
-
-  darwinPkgs =
-    import ./nix/nixpkgs.nix { inherit config overlays; system = "x86_64-darwin"; };
+    overlays = [
+      (import ./nix/overlays/haskell.nix { inherit compiler enableDhall; })
+    ];
+  };
 
 in
   { inherit (pkgs.haskell.packages."${compiler}")
