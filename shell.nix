f598a6d6b106268f6cc1ee297c5a4de32e2d2531
Evan Relf <evan@awakesecurity.com>
2021-06-22 11:17:25 -0700

    Speed up shell by optionally forgoing test deps

--- shell.nix
+++ shell.nix
@@ -1,1 +1,16 @@
-(import ./default.nix { }).proto3-suite.env
+# Entering the shell for `proto3-suite` requires rebuilding `proto3-suite`
+# when basically _any_ file changes, which makes development painful.
+#
+# If you don't need to run the test suite, you can forgo test dependencies
+# and skip `compile-proto-file` codegen by running:
+#
+#   $ nix-shell --arg fast true
+#
+{ fast ? false }:
+
+with (import ./default.nix { });
+
+if fast then
+  proto3-suite-boot.env
+else
+  proto3-suite.env
