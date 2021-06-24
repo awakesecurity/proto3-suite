c460a8a41fd60bfa1014017a7e60ded75aed98e9
Evan Relf <evan@awakesecurity.com>
2021-06-24 14:24:03 -0700

    Create multi-package Nix shell

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ compiler ? "ghc884"
+, enableDhall ? false
+, enableSwagger ? true
+}:
+
+let
+  pkgs = import ./nix/pkgs.nix {
+    inherit compiler enableDhall enableSwagger;
+  };
+
+in
+pkgs.haskellPackages.shellFor {
+  packages = p: [
+    p.proto3-suite
+    p.proto3-suite-compile-base
+  ];
+
+  buildInputs = [
+    pkgs.cabal-install
+    pkgs.ghcid
+  ];
+}
