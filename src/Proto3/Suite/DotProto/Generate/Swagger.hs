a8a703283b2bbfd1392e0b35fb456e72e860269e
Evan Relf <evan@awakesecurity.com>
2021-07-21 09:53:13 -0700

    Use Swagger schema `format` to distinguish wrapper types from primitives (#167)

--- src/Proto3/Suite/DotProto/Generate/Swagger.hs
+++ src/Proto3/Suite/DotProto/Generate/Swagger.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE CPP                 #-}
 {-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE MagicHash           #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -27,14 +28,18 @@ import           Data.Aeson                      (Value (String), ToJSONKey,
                                                   ToJSONKeyFunction(..))
 import qualified Data.Aeson                      as Aeson
 import           Data.Aeson.Encode.Pretty        (encodePretty)
-import           Data.ByteString                 (ByteString)
+import qualified Data.ByteString                 as B
+import qualified Data.ByteString.Lazy            as BL
 import qualified Data.ByteString.Lazy.Char8      as LC8
+import           Data.Functor                    ((<&>))
 import           Data.Hashable                   (Hashable)
 import           Data.HashMap.Strict.InsOrd      (InsOrdHashMap)
 import qualified Data.HashMap.Strict.InsOrd
 import           Data.Map                        (Map)
 import           Data.Swagger
+import           Data.Swagger.Declare            (Declare)
 import qualified Data.Text                       as T
+import qualified Data.Text.Lazy                  as TL
 import           Data.Proxy
 import qualified Data.Vector                     as V
 import           GHC.Exts                        (Proxy#, proxy#)
@@ -56,15 +61,10 @@ newtype OverrideToSchema a = OverrideToSchema { unOverride :: a }
 instance {-# OVERLAPPABLE #-} ToSchema a => ToSchema (OverrideToSchema a) where
   declareNamedSchema _ = declareNamedSchema (Proxy :: Proxy a)
 
-instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema ByteString) where
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema B.ByteString) where
   declareNamedSchema _ = return (NamedSchema Nothing byteSchema)
 
--- | This instance is the same as the instance for @OverrideToSchema ByteString@.
--- See: https://hackage.haskell.org/package/swagger2-2.6/docs/src/Data.Swagger.Internal.Schema.html#line-451
-instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe ByteString)) where
-  declareNamedSchema _ = return (NamedSchema Nothing byteSchema)
-
-instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (V.Vector ByteString)) where
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (V.Vector B.ByteString)) where
   declareNamedSchema _ = return (NamedSchema Nothing schema_)
     where
       schema_ = mempty
@@ -75,12 +75,12 @@ instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (V.Vector ByteString)) w
 #endif
         & items ?~ SwaggerItemsObject (Inline byteSchema)
 
-instance {-# OVERLAPPING #-} (ToJSONKey k, ToSchema k) => ToSchema (OverrideToSchema (Map k ByteString)) where
+instance {-# OVERLAPPING #-} (ToJSONKey k, ToSchema k) => ToSchema (OverrideToSchema (Map k B.ByteString)) where
   declareNamedSchema _ = case Aeson.toJSONKey :: ToJSONKeyFunction k of
       ToJSONKeyText _ _ -> do
           return (NamedSchema Nothing schema_)
       ToJSONKeyValue _ _ -> do
-          declareNamedSchema (Proxy :: Proxy [(k, (OverrideToSchema ByteString))])
+          declareNamedSchema (Proxy :: Proxy [(k, (OverrideToSchema B.ByteString))])
     where
       schema_ = mempty
 #if MIN_VERSION_swagger2(2,4,0)
@@ -89,6 +89,66 @@ instance {-# OVERLAPPING #-} (ToJSONKey k, ToSchema k) => ToSchema (OverrideToSc
         & type_ .~ SwaggerObject
 #endif
         & additionalProperties ?~ AdditionalPropertiesSchema (Inline byteSchema)
+
+#ifdef SWAGGER_WRAPPER_FORMAT
+-- | Wrapped Type Schemas
+
+setFormat
+  :: T.Text
+  -> Declare (Definitions Schema) NamedSchema
+  -> Declare (Definitions Schema) NamedSchema
+setFormat formatValue namedSchema =
+  namedSchema
+#if MIN_VERSION_swagger2(2,4,0)
+    <&> schema . paramSchema . format ?~ formatValue
+#else
+    <&> schema . paramSchema . format .~ formatValue
+#endif
+
+declareWrapperNamedSchema
+  :: forall a
+   . ToSchema a
+  => T.Text
+  -> Proxy (OverrideToSchema a)
+  -> Declare (Definitions Schema) NamedSchema
+declareWrapperNamedSchema formatValue _ =
+  setFormat formatValue (declareNamedSchema (Proxy :: Proxy a))
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe Double)) where
+  declareNamedSchema = declareWrapperNamedSchema "DoubleValue"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe Float)) where
+  declareNamedSchema = declareWrapperNamedSchema "FloatValue"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe Int64)) where
+  declareNamedSchema = declareWrapperNamedSchema "Int64Value"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe Word64)) where
+  declareNamedSchema = declareWrapperNamedSchema "UInt64Value"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe Int32)) where
+  declareNamedSchema = declareWrapperNamedSchema "Int32Value"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe Word32)) where
+  declareNamedSchema = declareWrapperNamedSchema "UInt32Value"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe Bool)) where
+  declareNamedSchema = declareWrapperNamedSchema "BoolValue"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe T.Text)) where
+  declareNamedSchema = declareWrapperNamedSchema "StringValue"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe TL.Text)) where
+  declareNamedSchema = declareWrapperNamedSchema "StringValue"
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe B.ByteString)) where
+  declareNamedSchema _ =
+    setFormat "BytesValue" (pure (NamedSchema Nothing byteSchema))
+
+instance {-# OVERLAPPING #-} ToSchema (OverrideToSchema (Maybe BL.ByteString)) where
+  declareNamedSchema _ =
+    setFormat "BytesValue" (pure (NamedSchema Nothing byteSchema))
+#endif
 
 {-| This is a convenience function that uses type inference to select the
     correct instance of `ToSchema` to use for fields of a message
