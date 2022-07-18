28b1c93148d775509c7826cc0303a9bdbf1e5189
j6carey <jcarey@arista.com>
2022-07-18 08:48:30 -0700

    Eliminate a number of compiler warnings. (#210)
    These all relate to Turtle functions deprecated by turtle-1.6.
    
    Eliminating the warnings seems to make doctest behave more predictably.
    At the very least it avoids cluttering doctest and compiler output.
    
    We could use -Wno-deprecations, but this change is more selective.

---
+++ src/Turtle/Compat.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE CPP #-}
+
+-- | Compatibility module to avoid compiler warnings about deprecation
+-- of obsolete Turtle functionality related to FilePath.  Once we
+-- no longer support pre-1.6 Turtle we can eliminate this module.
+module Turtle.Compat
+  ( absolute
+  , collapse
+  , encodeString
+  , fromText
+  , toText
+  ) where
+
+import qualified Data.Text
+import qualified System.FilePath
+import qualified Turtle
+
+absolute :: FilePath -> Bool
+collapse :: Turtle.FilePath -> Turtle.FilePath
+encodeString :: Turtle.FilePath -> String
+fromText :: Turtle.Text -> Turtle.FilePath
+toText :: Turtle.FilePath -> Either Turtle.Text Turtle.Text
+
+#if MIN_VERSION_turtle(1,6,0)
+
+absolute = System.FilePath.isAbsolute
+collapse = System.FilePath.normalise
+encodeString = id
+fromText = Data.Text.unpack
+toText = Right . Data.Text.pack
+
+#else
+
+absolute = Turtle.absolute
+collapse = Turtle.collapse
+encodeString = Turtle.encodeString
+fromText = Turtle.fromText
+toText = Turtle.toText
+
+#endif
