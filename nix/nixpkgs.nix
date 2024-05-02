fdebd9bb77150f9763f700379a9e67e630f1bcf7
John Carey <jcarey@arista.com>
2024-05-02 16:31:06 -0700

    Switch to nixpkgs-23.11, which has GHC 9.6.2.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -2,9 +2,9 @@ args:
 
 let
   nixpkgs = builtins.fetchTarball {
-    # release: nixpkgs-23.05pre491123.261abe8a44a7
-    # commit: 261abe8a44a7e8392598d038d2e01f7b33cf26d0
-    url = "https://hydra.nixos.org/build/236149912/download/2/nixpkgs-23.05pre491123.261abe8a44a7.tar.xz";
-    sha256 = "0yhf6zbnkj3a7wfas5clli5qk4xl0cw5zq5w4fzvd724za5nb04f";
+    # build: https://hydra.nixos.org/build/258096332
+    # commit: 0638fe2715d998fa81d173aad264eb671ce2ebc1
+    url = "https://hydra.nixos.org/build/258096332/download/2/nixpkgs-23.11pre558121.0638fe2715d9.tar.xz";
+    sha256 = "1z3s1hqg3b72g608pf9sv474d4y9s00p86nsvfw5i9xgwhjncjjb";
   };
 in import nixpkgs ({ config = { }; } // args)
