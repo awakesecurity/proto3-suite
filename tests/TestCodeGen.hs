8d2d17e59a423f5e720e8eb44512c255e41a0c30
jake <jacobleach@protonmail.com>
2024-11-07 16:31:50 -0600

    Fix generation of negative enum fields (#258)
    Fixes an issue where enums with negative field
    numbers were compiled to the field's conjugate
    in toProtoEnumMay pattern matches.
    
    This commit also adds tests of negative enum codes
    based on interoperability with generated Python.

--- tests/TestCodeGen.hs
+++ tests/TestCodeGen.hs
@@ -255,6 +255,7 @@ compileTestDotProtos logger recStyle decodedStringType = do
       protoFiles =
         [ "test_proto.proto"
         , "test_proto_import.proto"
+        , "test_proto_negative_enum.proto"
         , "test_proto_oneof.proto"
         , "test_proto_oneof_import.proto"
         {- These tests have been temporarily removed to pass CI.
@@ -263,6 +264,7 @@ compileTestDotProtos logger recStyle decodedStringType = do
         -}
         , "test_proto_nested_message.proto"
         , "test_proto_wrappers.proto"
+        , "test_proto_negative_enum.proto"
         ]
 
   forM_ protoFiles $ \protoFile -> do
