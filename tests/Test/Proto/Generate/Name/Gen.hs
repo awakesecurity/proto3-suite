924855464e5956f33f3438397cf5e27da55805b7
riz0id <jacob.leach@arista.com>
2025-11-12 13:23:38 -0600

    fix: imports

--- tests/Test/Proto/Generate/Name/Gen.hs
+++ tests/Test/Proto/Generate/Name/Gen.hs
@@ -13,6 +13,8 @@ module Test.Proto.Generate.Name.Gen
   )
 where
 
+import Control.Applicative (liftA2)
+
 import Hedgehog (MonadGen, Range)
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
