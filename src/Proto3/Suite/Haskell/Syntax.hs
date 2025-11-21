5f5da152ff6ec8e1e1fd2c2e5db9505abe028240
riz0id <jacob.leach@arista.com>
2025-11-21 16:15:32 -0600

    add: AST converters

--- src/Proto3/Suite/Haskell/Syntax.hs
+++ src/Proto3/Suite/Haskell/Syntax.hs
@@ -3,12 +3,46 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module Proto3.Suite.Haskell.Syntax
-  ( -- * HsModule
-    HsModule
-    -- * HsDecl
-  , HsDecl
+  ( -- * HsDecl
+    hsDeclToDec
+    -- * InstDecl
+  , instDeclToDec
     -- * ClsInstDecl
-  , ClsInstDecl
+  , clsInstDeclToDec
+    -- * HsBindLR
+  , hsBindLR
+    -- * MatchGroup
+  , matchGroupToClause  
+    -- * Match
+  , hsMatchToClause
+    -- * HsExpr
+  , hsExprToExp
+    -- * PatSynBind
+  , patSynBindToDec 
+    -- * HsLit
+  , hsLitToLit
+    -- * IntegralLit
+  , integralLitToInteger
+    -- * Pat
+  , hsPatToPat
+    -- * HsConPatTyArg
+  , hsConPatTyArgToType
+    -- * HsRecFields
+  , hsRecFieldsToFieldPat
+    -- * HsFieldBind
+  , hsFieldBindToFieldPat
+    -- * HsPatSigType
+  , hsPatSigTypeToType
+    -- * HsTyPat
+  , hsTyPatToType 
+    -- * OverlapMode
+  , overlapModeToOverlap
+    -- * HsSigType
+  , hsSigTypeToType
+    -- * HsType
+  , hsTypeToType
+    -- * RdrName
+  , rdrNameToName
   ) where
 
 import Data.ByteString.Char8 qualified as ByteString
@@ -22,13 +56,34 @@ import "ghc-lib-parser" Language.Haskell.Syntax.Binds qualified as GHC
 import "ghc-lib-parser" Language.Haskell.Syntax.Type qualified as GHC
   ( FieldOcc (..)
   )
+import "ghc-lib-parser" Language.Haskell.Syntax.Binds qualified as GHC
+  ( HsLocalBindsLR (..)
+  , HsValBindsLR (..)
+  , HsIPBinds (..)
+  )
+import "ghc-lib-parser" GHC.Unit.Types qualified as GHC
+  ( Module (..)
+  , moduleName
+  , moduleUnitId
+  )
+import "ghc-lib-parser" Language.Haskell.Syntax.Expr qualified as GHC
+  ( GRHSs (..)
+  , GRHS (..)
+  , StmtLR (..)
+  )
 import "ghc-lib-parser" Language.Haskell.Syntax qualified as GHC
   ( ClsInstDecl (..)
   , HsBindLR (..)
+  , HsLamVariant (..)
+  , HsArrowMatchContext (..)
+  , HsMatchContext (..) 
+  , HsConPatTyArg (..)
+  , HsTyPat (..)
   , HsRecFields (..)
   , HsFieldBind (..)
   , HsDecl (..)
   , HsExpr (..)
+  , LHsExpr 
   , HsModule (..)
   , HsConDetails (..)
   , HsConPatDetails (..)
@@ -41,6 +96,7 @@ import "ghc-lib-parser" Language.Haskell.Syntax qualified as GHC
   , HsPatSynDetails 
   , HsUntypedSplice (..)
   , LHsFieldBind
+  , LFieldOcc
   , HsLit (..)
   , GRHSs
   , IdP (..)
@@ -57,6 +113,7 @@ import "ghc-lib-parser" Language.Haskell.Syntax qualified as GHC
 import "ghc-lib-parser" GHC.Parser.Annotation qualified as GHC
   ( SrcSpanAnnA
   )
+import "ghc-lib-parser" GHC.Types.Name.Reader qualified as GHC
 import "ghc-lib-parser" GHC.Types.Name.Reader qualified as GHC
   ( RdrName (..)
   , rdrNameOcc
@@ -138,14 +195,121 @@ hsBindLR (GHC.PatSynBind _x bind) = patSynBindToDec bind
 hsBindLR (GHC.FunBind _x idt matches) = 
   TH.FunD
     (rdrNameToName (unLoc idt))
-    (_ matches)
+    (matchGroupToClause matches)
 hsBindLR (GHC.PatBind _x lhs mult rhs) = 
-  _
+  error "hsBindLR: PatBind not yet implemented"
 hsBindLR (GHC.VarBind _x idt rhs) = 
   TH.ValD
     (TH.VarP (rdrNameToName idt))
     (TH.NormalB (hsExprToExp (unLoc rhs)))
     []
+
+--- MatchGroup -----------------------------------------------------------------
+
+matchGroupToClause :: GHC.MatchGroup GHC.GhcPs (GHC.LHsExpr GHC.GhcPs) -> [TH.Clause]
+matchGroupToClause GHC.MG {..} = map (hsMatchToClause . unLoc) (unLoc mg_alts)
+
+--- Match ----------------------------------------------------------------------
+
+hsMatchToClause :: GHC.Match GHC.GhcPs (GHC.LHsExpr GHC.GhcPs) -> TH.Clause
+hsMatchToClause (GHC.Match _x ctxt pats grRHS) = 
+  TH.Clause 
+    thPats 
+    (TH.GuardedB (foldr ((++) . conv . unLoc) [] (GHC.grhssGRHSs grRHS)))
+    (bindsToDecs (GHC.grhssLocalBinds grRHS))
+  where 
+    thPats :: [TH.Pat]
+    thPats = map (hsPatToPat . unLoc) pats
+
+    conv :: GHC.GRHS GHC.GhcPs (GHC.LHsExpr GHC.GhcPs) -> [(TH.Guard, TH.Exp)]
+    conv (GHC.GRHS _x guards body) = 
+      [(stmtLRToGuard (unLoc guard), hsExprToExp (unLoc body)) | guard <- guards]
+
+    bindsToDecs :: GHC.HsLocalBindsLR GHC.GhcPs GHC.GhcPs -> [TH.Dec]
+    bindsToDecs (GHC.HsValBinds _x (GHC.ValBinds _x2 binds sigs)) = 
+      map hsBindLR (foldr ((:) . unLoc) [] binds)
+    bindsToDecs (GHC.HsIPBinds _x (GHC.IPBinds _x2 hsIPBinds)) = 
+      error "hsMatchToClause: HsIPBinds not yet implemented"
+    bindsToDecs (GHC.EmptyLocalBinds _x) = 
+      []
+
+    -- thDecs :: [TH.Dec]
+    -- thDecs = case GHC.grhssLocalBinds grRHS of 
+    --   GHC.HsValBinds _x1 (GHC.ValBinds _x2 binds sigs) -> 
+    --     _
+    --   GHC.HsIPBinds _x1 (GHC.IPBinds _x2 hsIPBinds) -> 
+    --     _
+    --   GHC.EmptyLocalBinds _x -> 
+    --     []
+
+gRhsToGuardedExp :: GHC.GRHS GHC.GhcPs (GHC.LHsExpr GHC.GhcPs) -> ([TH.Guard], TH.Exp)
+gRhsToGuardedExp (GHC.GRHS _x guards body) = (thGuards, thExp) 
+  where 
+    thGuards :: [TH.Guard]
+    thGuards = map (stmtLRToGuard . unLoc) guards
+
+    thExp :: TH.Exp
+    thExp = hsExprToExp (unLoc body)
+
+stmtLRToGuard :: GHC.StmtLR GHC.GhcPs GHC.GhcPs (GHC.LHsExpr GHC.GhcPs) -> TH.Guard
+stmtLRToGuard (GHC.BindStmt _x pat body) = 
+  TH.PatG [TH.BindS (hsPatToPat (unLoc pat)) (hsExprToExp (unLoc body))]
+stmtLRToGuard (GHC.BodyStmt _x body _stxl _stxr) = 
+  TH.NormalG (hsExprToExp (unLoc body))
+stmtLRToGuard (GHC.LetStmt _x locals) = 
+  error ("'LetStmt' statement in guard: not supported; no body expression")
+stmtLRToGuard (GHC.LastStmt _x body mb _stx) = 
+  error ("'LastStmt' statement in guard: not supported; no body expression")
+stmtLRToGuard (GHC.ParStmt _x parStmtBlock hsExpr _stx) = 
+  error ("'ParStmt' statement in guard: not supported")
+stmtLRToGuard (GHC.TransStmt _x form stmts bndrs usingExpr byExpr _stxret _stxbind fmapExpr) = 
+  error ("'TransStmt' statement in guard: not supported")
+stmtLRToGuard (GHC.RecStmt _x stmts later_ids rec_ids _stxbind_fn _stxret_fn _stxmfix_fn) = 
+  error ("'RecStmt' statement in guard: not supported")
+
+-- hsMatchToClause GHC.Match {..} = case m_ctxt of 
+--   GHC.FunRhs fun fixity strictness -> 
+--     _
+--   GHC.CaseAlt -> 
+--     _
+--   GHC.LamAlt variant -> case variant of 
+--     GHC.LamSingle -> 
+--       _
+--     GHC.LamCase -> 
+--       _
+--     GHC.LamCases -> 
+--       _
+--   GHC.IfAlt -> 
+--     _
+--   GHC.ArrowMatchCtxt arrowMatchCtxt -> case arrowMatchCtxt of
+--     GHC.ProcExpr -> 
+--       _
+--     GHC.ArrowCaseAlt -> 
+--       _ 
+--     GHC.ArrowLamAlt variant -> case variant of 
+--       GHC.LamSingle -> 
+--         _
+--       GHC.LamCase -> 
+--         _
+--       GHC.LamCases -> 
+--         _
+--   where 
+    -- hsMatchToClause GHC.Match {..} =
+
+  -- TH.Clause 
+  --   (map (hsPatToPat . unLoc) m_pats)
+  --   (_ m_ctxt)
+  --   (gRHSsToDec m_grhss)
+
+--- HsMatchContext -------------------------------------------------------------
+
+-- hsMatchContextToMatchContext :: GHC.MatchContext -> TH.MatchContext
+
+
+--- GRHSs ----------------------------------------------------------------------
+
+-- gRHSsToDec :: GHC.GRHSs GHC.GhcPs (GHC.LHsExpr GHC.GhcPs) -> [TH.Dec]
+-- gRHSsToDec GHC.GRHSs {..} = _
 
 --- HsExpr ---------------------------------------------------------------------
 
@@ -160,37 +324,12 @@ hsPatSynDirToPatSynDir GHC.Unidirectional = TH.Unidir
 hsPatSynDirToPatSynDir GHC.ImplicitBidirectional = TH.ImplBidir
 hsPatSynDirToPatSynDir (GHC.ExplicitBidirectional expr) = TH.ExplBidir (map (hsMatchToClause . unLoc) (unLoc (GHC.mg_alts expr)))
 
---- HsConDetails ------------------------------------------------------------
-
-hsConPatDetailsToPatSynDetails :: GHC.HsConPatDetails GHC.GhcPs -> TH.PatSynArgs
-hsConPatDetailsToPatSynDetails (GHC.PrefixCon _tyArgs args) = 
-  TH.PrefixPatSyn 
-    (map (rdrNameToName . unLoc) args)
-hsConPatDetailsToPatSynDetails (GHC.InfixCon lhs rhs) = 
-  TH.InfixPatSyn
-    (_ lhs)
-    (_ rhs)
-hsConPatDetailsToPatSynDetails (GHC.RecCon r) 
-  | isWildCard = 
-    TH.RecordPatSyn (map (_ . unLoc) (GHC.rec_flds r))
-  | otherwise =
-    TH.RecordPatSyn (map (_ . unLoc) (GHC.rec_flds r))
-  where 
-    isWildCard :: Bool
-    isWildCard = isJust (GHC.rec_dotdot r)
-
 --- HsPatSynDetails ------------------------------------------------------------
 
 hsPatSynDetailsToPatSynDetails :: GHC.HsPatSynDetails GHC.GhcPs -> TH.PatSynArgs
 hsPatSynDetailsToPatSynDetails (GHC.PrefixCon _tyArgs args) = TH.PrefixPatSyn (map (rdrNameToName . unLoc) args)
 hsPatSynDetailsToPatSynDetails (GHC.InfixCon lhs rhs) = TH.InfixPatSyn (rdrNameToName (unLoc lhs)) (rdrNameToName (unLoc rhs))
 hsPatSynDetailsToPatSynDetails (GHC.RecCon r) = TH.RecordPatSyn (map (rdrNameToName . unLoc . GHC.recordPatSynPatVar) r)
-
---- GRHSs -----------------------------------------------------------------------
-
-hsMatchToClause :: GHC.Match GHC.GhcPs (GenLocated GHC.SrcSpanAnnA (GHC.HsExpr GHC.GhcPs)) -> TH.Clause
-hsMatchToClause = _
-
 
 --- PatSynBind -----------------------------------------------------------------
 
@@ -238,7 +377,7 @@ hsPatToPat (GHC.ConPat _x con details) = case details of
   GHC.PrefixCon tyArgs args ->
     TH.ConP
       (rdrNameToName (unLoc con))
-      (map _ tyArgs) 
+      (map hsConPatTyArgToType tyArgs) 
       (map (hsPatToPat . unLoc) args)
   GHC.InfixCon lhs rhs ->
     TH.InfixP
@@ -248,23 +387,30 @@ hsPatToPat (GHC.ConPat _x con details) = case details of
   GHC.RecCon r ->
     TH.RecP
       (rdrNameToName (unLoc con))
-      (hsRecFieldsToFieldPat (fmap unLoc r))
+      (error "RecP: NYI") -- (_ r) 
 hsPatToPat (GHC.ViewPat _x lhs rhs) = TH.ViewP (hsExprToExp (unLoc lhs)) (hsPatToPat (unLoc rhs))
 hsPatToPat (GHC.SplicePat _x _spl) = error "hsPatToPat: SplicePat not yet implemented"
 hsPatToPat (GHC.LitPat _x lit) = TH.LitP (hsLitToLit lit)
-hsPatToPat (GHC.NPat _x hsOverLit optStxExpr stxExpr) = error "hsPatToPat: n pattern not supportedf"
-hsPatToPat (GHC.NPlusKPat _x idt hsOverLit optStxExpr stxExpr x) = error "hsPatToPat: n plus k pattern not supported"
+hsPatToPat (GHC.NPat _x _hsOverLit _optStxExpr _stxExpr) = error "hsPatToPat: n pattern not supportedf"
+hsPatToPat (GHC.NPlusKPat _x _idt _hsOverLit _optStxExpr _stxExpr _v) = error "hsPatToPat: n plus k pattern not supported"
 hsPatToPat (GHC.SigPat _x pat sig) = TH.SigP (hsPatToPat (unLoc pat)) (hsPatSigTypeToType sig)
 hsPatToPat (GHC.EmbTyPat _x tyPat) = TH.TypeP (hsTyPatToType tyPat)
 hsPatToPat (GHC.InvisPat _x tyPat) = TH.InvisP (hsTyPatToType tyPat)
 
+--- HsConPatTyArg --------------------------------------------------------------
+
+hsConPatTyArgToType :: GHC.HsConPatTyArg GHC.GhcPs -> TH.Type
+hsConPatTyArgToType (GHC.HsConPatTyArg _x tyPat) = hsTyPatToType tyPat
+
 --- HsRecFields ----------------------------------------------------------------
 
 hsRecFieldsToFieldPat :: GHC.HsRecFields GHC.GhcPs (GHC.Pat GHC.GhcPs) -> [TH.FieldPat]
-hsRecFieldsToFieldPat hsRecFields = map (conv . unLoc) (GHC.rec_flds hsRecFields)
+hsRecFieldsToFieldPat = map (hsFieldBindToFieldPat . unLoc) . GHC.rec_flds
 
-conv :: GHC.HsFieldBind (GenLocated GHC.SrcSpanAnnA (GHC.FieldOcc GHC.GhcPs)) (GHC.Pat GHC.GhcPs) -> TH.FieldPat
-conv hsFieldBind = (fieldIdt, fieldPat)
+--- HsFieldBind ----------------------------------------------------------------
+
+hsFieldBindToFieldPat :: GHC.HsFieldBind (GHC.LFieldOcc GHC.GhcPs) (GHC.Pat GHC.GhcPs) -> TH.FieldPat
+hsFieldBindToFieldPat hsFieldBind = (fieldIdt, fieldPat)
   where 
     fieldIdt :: TH.Name
     fieldIdt = rdrNameToName (unLoc (GHC.foLabel (unLoc (GHC.hfbLHS hsFieldBind))))
@@ -272,26 +418,10 @@ conv hsFieldBind = (fieldIdt, fieldPat)
     fieldPat :: TH.Pat
     fieldPat = hsPatToPat (GHC.hfbRHS hsFieldBind)
 
---- HsFieldBind ----------------------------------------------------------------
-
--- hsFieldBindToFieldPat :: LHsFieldBind -> TH.FieldPat
--- hsFieldBindToFieldPat GHC.HsFieldBind {..} = (fieldIdt, fieldPat)
---   where 
---     fieldIdt :: TH.Name
---     fieldIdt = _ 
-
---     fieldPat :: TH.Pat
---     fieldPat = _ 
-
---- HsConDetails ---------------------------------------------------------------
-
-hsConDetailsToPat :: GHC.HsConPatDetails GHC.GhcPs -> TH.Pat
-hsConDetailsToPat (GHC.PrefixCon _tys args) = TH.TupP (map (hsPatToPat . unLoc) args)
-
 --- HsPatSigType ---------------------------------------------------------------
 
 hsPatSigTypeToType :: GHC.HsPatSigType GHC.GhcPs -> TH.Type
-hsPatSigTypeToType (GHC.HsPS _x body) = hsTypeToType (unLoc body)
+hsPatSigTypeToType = hsTypeToType . unLoc . GHC.hsps_body 
 
 --- HsTyPat --------------------------------------------------------------------
 
@@ -324,12 +454,19 @@ hsTypeToType hsType = case hsType of
 
 rdrNameToName :: GHC.RdrName -> TH.Name
 rdrNameToName rdrName = case rdrName of
-  GHC.Unqual occName -> 
-    TH.mkName occNameStr
-  GHC.Exact name -> 
-    TH.mkName occNameStr
+  GHC.Unqual occName -> TH.mkName (GHC.occNameString occName) 
+  GHC.Exact name -> rdrNameToName (GHC.nameRdrName name)
+  GHC.Orig mdl occName -> 
+    TH.Name
+      (TH.OccName (GHC.occNameString occName)) 
+      (TH.NameG TH.VarName _ (ghcModuleNameToTHModuleName mdl))
   GHC.Qual moduleName occName -> 
-    TH.Name (TH.OccName occNameStr) (TH.NameQ (TH.ModName (GHC.moduleNameString moduleName)))
+    TH.Name 
+      (TH.OccName (GHC.occNameString occName)) 
+      (TH.NameQ (TH.ModName (GHC.moduleNameString moduleName)))
   where 
-    occNameStr :: String
-    occNameStr = GHC.occNameString (GHC.rdrNameOcc rdrName)
+    ghcModuleNameToUnit :: GHC.Module -> TH.PkgName
+    ghcModuleNameToUnit mdl = TH.PkgName (_ (GHC.moduleUnitId mdl))
+
+    ghcModuleNameToTHModuleName :: GHC.Module -> TH.ModName
+    ghcModuleNameToTHModuleName mdl = TH.ModName (GHC.moduleNameString (GHC.moduleName mdl))
