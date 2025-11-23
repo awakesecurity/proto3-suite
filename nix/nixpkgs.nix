50b1f75a5bc9c20f4f9b00d0d80cda6bf041a1ef
Tristan Cacqueray <tdecacqu@redhat.com>
2025-11-23 08:24:31 +0100

    Update nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -2,10 +2,11 @@ args:
 
 let
   nixpkgs = builtins.fetchTarball {
-    # from: https://releases.nixos.org/nixos/unstable/nixos-25.11pre868392.e9f00bd89398
-    # build: https://hydra.nixos.org/build/308438307
-    # commit: e9f00bd893984bc8ce46c895c3bf7cac95331127
-    url = "https://hydra.nixos.org/build/308438307/download/2/nixpkgs-25.11pre868392.e9f00bd89398.tar.xz";
-    sha256 = "1n08xvypbyygjzrrkyna06sjbxg987jvmvdib5qmzp8avx6dlprb";
+    # from: https://hydra.nixos.org/job/nixos/trunk-combined/nixpkgs.tarball
+    # build: https://hydra.nixos.org/build/314100404
+    # commit: 117cc7f94e8072499b0a7aa4c52084fa4e11cc9b
+    url =
+      "https://hydra.nixos.org/build/314100404/download/2/nixpkgs-25.11pre899548.117cc7f94e80.tar.xz";
+    sha256 = "1zjs02gl3kjlyx65m9jz5pm3f08s8pm1wbsx3h99cifvqbl6gb2a";
   };
 in import nixpkgs ({ config = { }; } // args)
